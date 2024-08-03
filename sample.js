ac226ce5ec574655dcea9a88a7b06dc9daff719a
Siddhu863 <147127968+Siddhu863@users.noreply.github.com>
2024-08-03 17:01:14 +0530

    Add files via upload

---
+++ sample.js
@@ -0,0 +1,51 @@
+// let a;
+// a = 20;
+// console.log(typeof (a));
+
+let man
+man = {
+    name:"siddhu",
+    dob:"3112005"
+}
+// console.log(man.dob)
+// console.log(man)
+// console.log(man.name||"data found")
+// console.log(man.name&&"data found")
+//console.log(!!(man.name))
+//  if(!man.name){
+//      console.log(man.name)
+//  }else(man.dob)
+//  {
+//     console.log(man.dob)
+//  }
+// let num=1
+// switch(num)
+// {
+//     case 11:console.log(man.name)
+//            break;
+//     case 2:console.log(man.dob)       
+//            break;
+//     default:console.log("no data")       
+// }
+// let toggle={
+
+// }
+// let arr=[12,43,776,35,32]
+
+// for(let i=0;i<5;i++){
+//     console.log(arr[i])
+// }
+// for(let c in arr){
+//     console.log(c)
+// }
+// for(let c of arr){
+//     console.log(c,)
+// }
+// for(let c in arr){
+//     console.log(c,arr[c])
+// }
+// let i=1
+// while(i<5){
+//     console.log(i)
+//     i++
+// }
